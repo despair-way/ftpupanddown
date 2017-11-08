6a40e9ee89fcae5ad17ebae86cfe27381af3eee1
zhangyuanzhen <zhangyz@jiurongtech.com>
2017-11-08 09:55:06 +0800

    test文件

---
+++ src/main/java/com/jiurong/autotransfer/test.java
@@ -0,0 +1,28 @@
+package com.jiurong.autotransfer;
+
+
+import java.io.InputStream;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jiurong.autotransfer.config.TransferConfig;
+public class test {
+
+	
+	public static void main(String[] args) throws Exception {
+//		File file = new File("C:/download/upload/test.txt");
+//		System.out.println(file.getName());
+//		System.out.println(file.getParent());
+//
+//		file.getName();
+//		file.getParent();
+//		
+//		
+//		
+		InputStream content = TransferConfig.class.getClassLoader().getResourceAsStream("app.json");
+		ObjectMapper objectMapper = new ObjectMapper();
+		TransferConfig config = objectMapper.readValue(content, TransferConfig.class);
+//		System.out.println(config.getSource());
+		
+		FTPUpAndDown.ftpUp(FTPUpAndDown.UpInfo(config));
+	}
+}
+
